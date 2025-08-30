38a125fb27361c13aac170be079e73f2492dfe82
AkriloCreations <akrilocreations@gmail.com>
2025-08-31 02:15:02 +0700

    enhance.api.py

---
+++ shopee_bridge/shopee_bridge/doctype/shopee_webhook_log/shopee_webhook_log.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, AkriloCreations and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Shopee Webhook Log", {
+// 	refresh(frm) {
+
+// 	},
+// });
